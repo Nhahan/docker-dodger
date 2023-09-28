9a30d5ee04387832ad6ad47563d14e03133982b7
Nhahan <kisy324@naver.com>
2023-09-28 16:59:28 +0900

    feat: scene bases

---
+++ src/js/Loading.js
@@ -0,0 +1,35 @@
+import Phaser from 'phaser';
+
+export default class Loading extends Phaser.Scene {
+
+    constructor() {
+        super('loading');
+    }
+
+    preload() {
+    }
+
+    create() {
+    }
+
+    update() {
+    }
+
+    ending(text) {
+    }
+
+    //게임오버
+    gameOver() {
+    }
+
+    //게임클리어
+    gameClear() {
+    }
+
+    destroyUnit(target) {
+    }
+
+    addScore(value) {
+    }
+
+}
---
+++ src/js/Round.js
@@ -0,0 +1,35 @@
+import Phaser from 'phaser';
+
+export default class Round extends Phaser.Scene {
+
+    constructor() {
+        super('round');
+    }
+
+    preload() {//사전설정
+    }
+
+    create() {//생성
+    }
+
+    update() {
+    }
+
+    ending(text) {
+    }
+
+    //게임오버
+    gameOver() {
+    }
+
+    //게임클리어
+    gameClear() {
+    }
+
+    destroyUnit(target) {
+    }
+
+    addScore(value) {
+    }
+
+}
