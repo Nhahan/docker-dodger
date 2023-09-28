610c21f70f33ab479dd6625bf0005d95d62e6deb
Nhahan <kisy324@naver.com>
2023-09-28 18:31:18 +0900

    chore: missing webpack settings

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -37,8 +37,8 @@ module.exports = {
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
-      title: 'Acrobatic Evasion',
-      favicon: paths.src + '/images/favicon.png',
+      title: 'Docker Dodger',
+      favicon: paths.src + '/images/player.png',
       template: paths.src + '/template.html', // template file
       filename: 'index.html', // output file
     }),
--- src/js/Round.js
+++ src/js/Round.js
@@ -1,18 +1,30 @@
 import Phaser from 'phaser';
 
+import player from '@/images/player.png';
+
+import shader from '@/shaders/glow.glsl';
+
 export default class Round extends Phaser.Scene {
 
     constructor() {
         super('round');
     }
 
-    preload() {//사전설정
+    preload() {
+        this.load.image('player', player);
+
+        this.load.glsl('customShader', shader);
     }
 
-    create() {//생성
+    create() {
+        this.playerImage = this.add.image(400, 300, 'player');
+        this.shader = this.add.shader('customShader', this.playerImage.x, this.playerImage.y, 50, 50);
+        this.playerImage.mask = new Phaser.Display.Masks.BitmapMask(this, this.shader);
+
     }
 
     update() {
+        this.shader.setPosition(this.playerImage.x, this.playerImage.y);
     }
 
     ending(text) {
